a6c08a04e499fb7dfb2bad0d38a15c82120b3944
Farkas Csaba <farkascsabi123@gmail.com>
2025-07-28 22:58:29 +0200

    feat: add command to insert Point

--- MiniCad.cpp
+++ MiniCad.cpp
@@ -1,20 +1,23 @@
-// MiniCad.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <memory>
+#include "Shape.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<std::shared_ptr<Shape>> shapes;
+
+    while (true) {
+        std::cout << "Commands: addpoint x y | addline x1 y1 x2 y2 | draw | exit\n";
+        std::cout << "Enter command: ";
+
+        std::string command;
+        std::cin >> command;
+
+        if (command == "addpoint") {
+            int x, y;
+            std::cin >> x >> y;
+            shapes.push_back(std::make_shared<Point>(x, y));
+            std::cout << "Point added.\n";
+        }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
