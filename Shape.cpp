ce37d85265fc6472d11cbdfd11f268a137d30ee9
Farkas Csaba <farkascsabi123@gmail.com>
2025-08-01 22:25:24 +0200

    Implemented Draw() and toString() for Rectangle and Circle

--- Shape.cpp
+++ Shape.cpp
@@ -20,3 +20,21 @@ void Line::draw() const {
 std::string Line::toString() const {
     return "Line(" + start.toString() + " -> " + end.toString() + ")";
 }
+
+Rectangle::Rectangle(Point tl, int w, int h) : topLeft(tl), width(w), height(h) {}
+
+void Rectangle::draw() const {
+    std::cout << "Draw Rectangle at " << topLeft.toString()
+        << " with width " << width << " and height " << height << "\n";
+}
+std::string Rectangle::toString() const {
+    return "Rectangle(" + topLeft.toString() + ", w=" + std::to_string(width) + ", h=" + std::to_string(height) + ")";
+}
+
+Circle::Circle(Point c, int r) : center(c), radius(r) {}
+void Circle::draw() const {
+    std::cout << "Draw Circle at " << center.toString() << " with radius " << radius << "\n";
+}
+std::string Circle::toString() const {
+    return "Circle(" + center.toString() + ", r=" + std::to_string(radius) + ")";
+}
