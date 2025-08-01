ce9fee59b3aaac7822e0f46963be06f6a107445c
Farkas Csaba <farkascsabi123@gmail.com>
2025-08-01 22:20:36 +0200

    Added classes: Rectangle and Circle

--- Shape.h
+++ Shape.h
@@ -6,7 +6,7 @@
 class Shape {
 public:
     virtual ~Shape() {}
-    virtual void draw() const = 0;  
+    virtual void draw() const = 0;
     virtual std::string toString() const = 0;
 };
 
@@ -25,4 +25,23 @@ public:
     void draw() const override;
     std::string toString() const override;
 };
+
+class Rectangle : public Shape {
+public:
+    Point topLeft;
+    int width, height;
+    Rectangle(Point tl, int w, int h);
+    void draw() const override;
+    std::string toString() const override;
+};
+
+class Circle : public Shape {
+public:
+    Point center;
+    int radius;
+    Circle(Point c, int r);
+    void draw() const override;
+    std::string toString() const override;
+};
+
 
